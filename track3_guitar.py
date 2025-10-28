0d4af2330d2aab7cd8d3a2af51ab613847de8fbb
ruixiu717-web <ruixiu717@gmail.com>
2025-10-28 10:16:53 +0800

    Create track3_guitar.py

---
+++ track3_guitar.py
@@ -0,0 +1,17 @@
+# New TunePad cell
+rest(31.5)  ### Wait untill the guitar track comes in
+playNote([60,64,67], beats = 2.5)
+rest(0.9)
+playNote([55,59,62], beats = 2.5)
+rest(1)
+playNote([57,60,64], beats = 4.5)
+playNote([52,55,59], beats = 1)
+rest(2.7)
+playNote([53,57,60], beats = 1.25)
+rest(2.5)
+playNote([60,64,67], beats = 1.55)
+rest(1.5)
+playNote([62,65,69], beats = 1)
+rest(2.45)
+playNote([55,59,62], beats = 1)
+rest(2)
