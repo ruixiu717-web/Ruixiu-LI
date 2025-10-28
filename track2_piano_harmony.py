70319654ce43008f6f382dbfa701dc902dd40a55
ruixiu717-web <ruixiu717@gmail.com>
2025-10-28 10:15:20 +0800

    Create track2_piano_harmony.py

---
+++ track2_piano_harmony.py
@@ -0,0 +1,12 @@
+rest (2)
+playNote (65, beats = 1.1)
+rest (2.2)
+playNote (64, beats = 1.1)
+rest (3.6)
+playNote (65, beats = 1.1)
+rest (1.8)
+playNote (64, beats = 1)
+rest (3.4)
+playNote ([53,57,60], beats= 1.1)  ###We press 3 keys at the same time to produce a harmony
+rest (14.4)
+rest(30)
